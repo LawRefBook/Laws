9db08f3aebfebcbdbbc2e09c7d048c5d60b3b091
RanKKI <youloveonlymeh@gmail.com>
2023-05-24 21:27:06 +1000

    feat: hash file list

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,12 +12,23 @@ function pack {
     echo "Packaging $1 to $output_zip"
     cd "$1" || exit
 
+    _hash=$(git log -n 1 --pretty=format:"%H"  -- . ':!scripts' ':!.*' ':!DLC' | awk -F" " '{printf "%s", $1}')
+    _hash_file="$output_zip.hash"
+
+    if [ -f $_hash_file ]; then
+        if [ "$_hash" == "$(cat $_hash_file)" ]; then
+            echo "No changes detected, skipping..."
+            return
+        fi
+    fi
+
     find . -type f \( -name ".*" -prune \) -o \( -name "scripts" -o -name "release" -o -name ".venv" -o -name ".git*" -o -name "DLC" \) -prune -o -exec zip -q "$output_zip" {} +
 
-    shasum -a 1 -b $output_zip | awk -F" " '{printf "%s", $1}'> $output_zip.sha1
+    echo $_hash > $_hash_file
 }
 
 cd ../
+current_path=$(pwd)
 pack "." "laws.zip"
 folder="./DLC"
 for dir in "$folder"*/*; do
@@ -25,4 +36,11 @@ for dir in "$folder"*/*; do
     if [ -d "$dir" ]; then
         pack $dir "$filename.zip"
     fi
+done
+
+# Generate dlc.txt
+cd $current_path
+rm ./release/dlc.txt
+for file in $(find ./release/DLC -name "*.zip"); do
+    echo "$(basename $file) $(cat $file.hash)" >> ./release/dlc.txt
 done
